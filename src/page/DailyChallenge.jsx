741e2aad37be3dfe19b8809d5e07344efc866ca6
rezen <rezen.khadgi@gmail.com>
2025-01-26 07:31:13 +0545

    fix layout

--- src/page/DailyChallenge.jsx
+++ src/page/DailyChallenge.jsx
@@ -1,12 +1,18 @@
 import Challenge from "../component/Challenges/Challenge";
+import DashboardChallenge from "../component/Challenges/DashboardChallenge";
 import Footer from "../component/Footer/Footer2";
 import Navbar from "../component/Navbar/Navbar";
+
+import styles from "./DailyChallenge.module.css";
 
 function DailyChallenge() {
   return (
     <>
       <Navbar />
-      <Challenge />
+      <div className={styles.container}>
+        <DashboardChallenge className={styles.challenges} />
+        <Challenge />
+      </div>
       <Footer />
     </>
   );
--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import DashboardChallenge from "../component/Challenges/DashboardChallenge";
 import Footer from "../component/Footer/Footer2";
 import TodayMood from "../component/MoodToday/MoodToday";
@@ -7,17 +8,52 @@ import Timer from "../component/timer/Timer";
 import styles from "./Dashboard.module.css";
 
 function Dashboard({ setAuth }) {
+  const [name, setName] = useState("");
+
+  async function getName() {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token || token === "undefined") {
+        setAuth(false);
+        throw new Error("No token found");
+      }
+
+      const response = await fetch("http://localhost:5000/moodTracker", {
+        method: "GET",
+        headers: { token },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch username.");
+      }
+
+      const parseRes = await response.json();
+      setName(parseRes.user_name);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  useEffect(() => {
+    getName();
+  }, []);
+
   return (
     <>
       <Navbar setAuth={setAuth} />
+      <div>
+        <h1 className={styles.title}>Dashboard</h1>
+        <h2 className={styles.titleName}>Hi, {name}!</h2>
+      </div>
       <div className={styles.container}>
-        <h1>Dashboard</h1>
-        <h2>Hi, Royan!</h2>
-        <DashboardChallenge className={styles.dashboardChallenge} />
-        <Timer className={styles.timer} />
-        <DashboardChallenge className={styles.dashboardChallenges} />
-        <Note className={styles.note} />
-        <TodayMood />
+        <div className={styles.containerLeft}>
+          <DashboardChallenge className={styles.dashboardChallenge} />
+          <DashboardChallenge className={styles.dashboardChallenge} />
+        </div>
+        <div className={styles.containerRight}>
+          <Timer className={styles.timer} />
+          <TodayMood />
+          <Note className={styles.note} />
+        </div>
       </div>
       <Footer />
     </>
